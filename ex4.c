90b67fdafe9de99155ffaec2e71956572a4d8da0
Uthumatik <uukwattage@gmail.com>
2025-03-01 08:47:03 +0100

    ex4 done

---
+++ ex4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+    srand(time(NULL));
+    
+    pid = fork();
+
+    if (pid == 0) {
+        int sleep_time = rand() % 5 + 1;
+        execlp("echo", "echo", "Bonjour", argv[1], NULL);
+        printf("[fils] PID: %d, PPID: %d\n", getpid(), getppid());
+        printf("[fils] Je m'endors pour %ds...\n", sleep_time);
+        sleep(sleep_time);
+        printf("[fils] Je suis réveillé !\n");
+        printf("*** FIN du processus fils (%d) ***\n", getpid());
+        exit(0);
+    } else {
+        printf("[père] PID: %d, PPID: %d\n", getpid(), getppid());
+        printf("[père] En attente du réveil de son fils...\n");
+        wait(NULL);
+        printf("[père] Mon fils s'est réveillé !\n");
+        printf("*** FIN du processus père (%d) ***\n", getpid());
+    }
+    return 0;
+}
